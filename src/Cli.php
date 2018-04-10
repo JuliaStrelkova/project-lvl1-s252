07f20006540809a3a8521fbc8cd7c270c5c3e74f
Julia Strelkova <strelkovajuli@gmail.com>
2018-04-10 17:02:41 +0300

    add game brain-even

--- src/Cli.php
+++ src/Cli.php
@@ -2,12 +2,30 @@
 
 namespace  BrainGames\Cli;
 
+use function BrainGames\BrainEven\checkParity;
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function run($string)
 {
     line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line('Hello, %s!', $name);
+
+    if ($string === 'brain-games') {
+        $name = askUserName();
+        line('Hello, %s!', $name);
+
+        return;
+    }
+
+    if ($string === 'brain-even') {
+        line('Answer "yes" if number even otherwise answer "no".');
+        checkParity(3);
+
+        return;
+    }
+}
+
+function askUserName()
+{
+    return prompt('May I have your name?');
 }
